dad77058facbdc543259f864087d53c564d2e5c7
westbot <82739021+westbot657@users.noreply.github.com>
2025-06-25 23:42:44 -0600

    fix (probably) issue #20

--- src/main/java/com/beatcraft/BeatCraftPreLaunch.java
+++ src/main/java/com/beatcraft/BeatCraftPreLaunch.java
@@ -1,10 +1,13 @@
 package com.beatcraft;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
 
 public class BeatCraftPreLaunch implements PreLaunchEntrypoint {
     @Override
     public void onPreLaunch() {
-        //System.loadLibrary("renderdoc"); // render debugging
+        if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
+            //System.loadLibrary("renderdoc"); // render debugging
+        }
     }
 }
