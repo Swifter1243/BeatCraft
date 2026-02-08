027e3d7db124fe60032b161897ae141b2cff44de
westbot <82739021+westbot657@users.noreply.github.com>
2026-02-07 23:34:13 -0700

    add default sabers to creative inventory

--- fabric/src/main/java/com/beatcraft/fabric/common/items/FabricItemsGroup.java
+++ fabric/src/main/java/com/beatcraft/fabric/common/items/FabricItemsGroup.java
@@ -1,12 +1,18 @@
 package com.beatcraft.fabric.common.items;
 
+import com.beatcraft.common.data.components.ModComponents;
 import com.beatcraft.common.items.ModItemGroup;
+import com.beatcraft.common.items.ModItems;
+import com.beatcraft.common.items.SaberItem;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
+import net.minecraft.core.component.DataComponentPatch;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
 public class FabricItemsGroup {
@@ -24,6 +30,18 @@ public class FabricItemsGroup {
         ItemGroupEvents.modifyEntriesEvent(ModItemGroup.GROUP_KEY).register(group -> {
             group.accept(FabricItems.SABER_ITEM);
             group.accept(FabricItems.HEADSET_ITEM);
+            var redSaber = new ItemStack(Holder.direct(FabricItems.SABER_ITEM), 1, DataComponentPatch.builder()
+                .set(ModComponents.SABER_COLOR_COMPONENT.get(), 12595248)
+                .set(ModComponents.AUTO_SYNC_COLOR.get(), 1)
+                .build()
+            );
+            var blueSaber = new ItemStack(Holder.direct(FabricItems.SABER_ITEM), 1, DataComponentPatch.builder()
+                .set(ModComponents.SABER_COLOR_COMPONENT.get(), 2122920)
+                .set(ModComponents.AUTO_SYNC_COLOR.get(), 1)
+                .build()
+            );
+            group.accept(redSaber);
+            group.accept(blueSaber);
         });
 
     }
--- neoforge/src/main/java/com/beatcraft/neoforge/common/items/NeoforgeItemsGroup.java
+++ neoforge/src/main/java/com/beatcraft/neoforge/common/items/NeoforgeItemsGroup.java
@@ -1,9 +1,14 @@
 package com.beatcraft.neoforge.common.items;
 
 import com.beatcraft.Beatcraft;
+import com.beatcraft.common.data.components.ModComponents;
+import com.beatcraft.common.items.SaberItem;
+import net.minecraft.core.Holder;
+import net.minecraft.core.component.DataComponentPatch;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -21,6 +26,18 @@ public class NeoforgeItemsGroup {
         .displayItems((params, output) -> {
             output.accept(NeoforgeItems.SABER_ITEM);
             output.accept(NeoforgeItems.HEADSET_ITEM);
+            var redSaber = new ItemStack(Holder.direct(NeoforgeItems.SABER_ITEM.asItem()), 1, DataComponentPatch.builder()
+                .set(ModComponents.SABER_COLOR_COMPONENT.get(), 12595248)
+                .set(ModComponents.AUTO_SYNC_COLOR.get(), 1)
+                .build()
+            );
+            var blueSaber = new ItemStack(Holder.direct(NeoforgeItems.SABER_ITEM.asItem()), 1, DataComponentPatch.builder()
+                .set(ModComponents.SABER_COLOR_COMPONENT.get(), 2122920)
+                .set(ModComponents.AUTO_SYNC_COLOR.get(), 1)
+                .build()
+            );
+            output.accept(redSaber);
+            output.accept(blueSaber);
         })
         .build()
     );
