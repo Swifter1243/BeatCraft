36faea642fb13ab635ece9560fcbfbac6c90f972
westbot <82739021+westbot657@users.noreply.github.com>
2026-04-30 20:23:16 -0600

    fix indentation

--- common/src/main/java/com/beatcraft/client/render/menu/EndScreenPanel.java
+++ common/src/main/java/com/beatcraft/client/render/menu/EndScreenPanel.java
@@ -34,24 +34,24 @@ public class EndScreenPanel extends MenuPanel<EndScreenData> {
         widgets.addAll(List.of(
             new TextWidget(FAILED, new Vector3f(0, -80, 0), 5),
             new ButtonWidget(
-                    new Vector3f(0, 200, 0), new Vector2f(250, 50),
-                    () -> {
-                        data.hudRenderer.controller.scene = HUDRenderer.MenuScene.SongSelect;
-                        // BeatmapAudioPlayer.unmuteVanillaMusic();
-                        InputSystem.unlockHotbar();
-                        // HUDRenderer.sendSceneSync();
-                    },
-                    new HoverWidget(
-                            new Vector3f(),
-                            new Vector2f(250, 50),
-                            List.of(
-                                    new GradientWidget(new Vector3f(0, 0, 0.005f), new Vector2f(250, 50), 0x7F222222, 0x22222222, 0)
-                            ),
-                            List.of(
-                                    new GradientWidget(new Vector3f(0, 0, 0.005f), new Vector2f(250, 50), 0x7F2260B0, 0x22226080, 0)
-                            )
+                new Vector3f(0, 200, 0), new Vector2f(250, 50),
+                () -> {
+                    data.hudRenderer.controller.scene = HUDRenderer.MenuScene.SongSelect;
+                    // BeatmapAudioPlayer.unmuteVanillaMusic();
+                    InputSystem.unlockHotbar();
+                    // HUDRenderer.sendSceneSync();
+                },
+                new HoverWidget(
+                    new Vector3f(),
+                    new Vector2f(250, 50),
+                    List.of(
+                        new GradientWidget(new Vector3f(0, 0, 0.005f), new Vector2f(250, 50), 0x7F222222, 0x22222222, 0)
                     ),
-                    new TextWidget(CONTINUE, new Vector3f(0, -20, 0.05f), 5)
+                    List.of(
+                        new GradientWidget(new Vector3f(0, 0, 0.005f), new Vector2f(250, 50), 0x7F2260B0, 0x22226080, 0)
+                    )
+                ),
+                new TextWidget(CONTINUE, new Vector3f(0, -20, 0.05f), 5)
             )
         ));
     }
@@ -61,34 +61,34 @@ public class EndScreenPanel extends MenuPanel<EndScreenData> {
 
         widgets.clear();
         widgets.addAll(List.of(
-                new TextWidget(CLEARED, new Vector3f(0, -80, 0), 5),
-                new TextWidget(GOOD_CUTS, new Vector3f(-280, 30, -0.01f), 2),
-                new TextWidget(SCORE, new Vector3f(0, 30, -0.01f), 2),
-                new TextWidget(RANK, new Vector3f(280, 30, -0.01f), 2),
-                new TextWidget(data.goodCuts + "/" + data.totalNotes, new Vector3f(-280, 50, -0.01f), 5),
-                new TextWidget(String.valueOf(data.score), new Vector3f(0, 50, -0.01f), 5),
-                new TextWidget(String.valueOf(data.rank), new Vector3f(280, 50, -0.01f), 5),
-                new TextWidget(() -> MAX_COMBO.getString() + " " + data.maxCombo, new Vector3f(-280, 90, -0.01f), 1.5f),
-                new ButtonWidget(
-                        new Vector3f(0, 200, 0), new Vector2f(250, 50),
-                        () -> {
-                            data.hudRenderer.controller.scene = HUDRenderer.MenuScene.SongSelect;
-                            // BeatmapAudioPlayer.unmuteVanillaMusic();
-                            InputSystem.unlockHotbar();
-                            // HUDRenderer.sendSceneSync();
-                        },
-                        new HoverWidget(
-                                new Vector3f(),
-                                new Vector2f(250, 50),
-                                List.of(
-                                        new GradientWidget(new Vector3f(0, 0, 0.005f), new Vector2f(250, 50), 0x7F222222, 0x22222222, 0)
-                                ),
-                                List.of(
-                                        new GradientWidget(new Vector3f(0, 0, 0.005f), new Vector2f(250, 50), 0x7F2260B0, 0x22226080, 0)
-                                )
-                        ),
-                        new TextWidget(CONTINUE, new Vector3f(0, -20, 0.05f), 5)
-                )
+            new TextWidget(CLEARED, new Vector3f(0, -80, 0), 5),
+            new TextWidget(GOOD_CUTS, new Vector3f(-280, 30, -0.01f), 2),
+            new TextWidget(SCORE, new Vector3f(0, 30, -0.01f), 2),
+            new TextWidget(RANK, new Vector3f(280, 30, -0.01f), 2),
+            new TextWidget(data.goodCuts + "/" + data.totalNotes, new Vector3f(-280, 50, -0.01f), 5),
+            new TextWidget(String.valueOf(data.score), new Vector3f(0, 50, -0.01f), 5),
+            new TextWidget(String.valueOf(data.rank), new Vector3f(280, 50, -0.01f), 5),
+            new TextWidget(() -> MAX_COMBO.getString() + " " + data.maxCombo, new Vector3f(-280, 90, -0.01f), 1.5f),
+            new ButtonWidget(
+                new Vector3f(0, 200, 0), new Vector2f(250, 50),
+                () -> {
+                    data.hudRenderer.controller.scene = HUDRenderer.MenuScene.SongSelect;
+                    // BeatmapAudioPlayer.unmuteVanillaMusic();
+                    InputSystem.unlockHotbar();
+                    // HUDRenderer.sendSceneSync();
+                },
+                new HoverWidget(
+                    new Vector3f(),
+                    new Vector2f(250, 50),
+                    List.of(
+                        new GradientWidget(new Vector3f(0, 0, 0.005f), new Vector2f(250, 50), 0x7F222222, 0x22222222, 0)
+                    ),
+                    List.of(
+                        new GradientWidget(new Vector3f(0, 0, 0.005f), new Vector2f(250, 50), 0x7F2260B0, 0x22226080, 0)
+                    )
+                ),
+                new TextWidget(CONTINUE, new Vector3f(0, -20, 0.05f), 5)
+            )
         ));
     }
 }
